2f0d4167f6665709cac4ce7ffc3abca871c6af0e
guineu <guineuu@pm.me>
2021-11-08 00:33:32 +0100

    Heroku 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+const PORT = process.env.PORT || 3030
 async function main() {
-  await app.app.listen(process.env.PORT || 3030);
+  await app.app.listen(PORT, () => console.log('App iniciada'));
 }
 main();
