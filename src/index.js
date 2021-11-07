8a2b8884f847659df6a32d1bc2d0b19b8871b78c
guineu <guineuu@pm.me>
2021-11-08 00:54:10 +0100

    Heroku 6

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const app = require('./app');
 async function main() {
-  app.app.listen(process.env.PORT || 5000)
+  app.app.listen(process.env.PORT || 5000, '0.0.0.0')
   console.log('App iniciada')
 }
-
 main();
