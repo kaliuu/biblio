56e66813b66c08da72527f3dda368a8481fc664e
guineu <guineuu@pm.me>
2021-11-08 00:47:22 +0100

    Heroku 5

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const app = require('./app');
-app.app.set('port', (process.env.PORT || 3030));
 async function main() {
-  await app.app.listen(app.app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.app.get('port'))
-  })
+  app.app.listen(process.env.PORT || 5000)
+  console.log('App iniciada')
 }
 
 main();
