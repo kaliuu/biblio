df534bfb5d42d54a7a23bb0cbc5e040c22da730c
guineu <guineuu@pm.me>
2021-11-08 01:37:57 +0100

    aaaaaaaaaaaa

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const appl = require('./app');
 
-app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
+appl.app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
   console.log("Server is running.");
 });
